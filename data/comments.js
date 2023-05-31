04d61d3e728361673dbdd8bbe071fb86d06beb12
qaisarhussain <qhussain960@gmail.com>
2023-05-31 13:34:54 +0500

    "initial commit"

---
+++ data/comments.js
@@ -0,0 +1,15 @@
+export const comments =[
+    {
+        id:1,
+        text:'commnt1'
+    },
+    {
+        id:2,
+        text:'commnt2'
+    },
+    {
+        id:3,
+        text:'commnt3'
+    },
+
+]
